209aabd0d756a2b6c05684bd5cf0501ab8a192c0
luciakasman <lucia.kasman@muttdata.ai>
2022-05-09 19:51:01 -0300

    try smth

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-poetry run alembic stamp head &
-poetry run alembic revision --autogenerate -m "New Migration" &
+poetry run alembic stamp head
+poetry run alembic revision --autogenerate -m "New Migration"
 poetry run alembic upgrade head
