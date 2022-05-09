04f9176e859c0b82e46360da85cabbd36a6e787c
luciakasman <lucia.kasman@muttdata.ai>
2022-05-09 14:39:19 -0300

    test alembic migration change

--- alembic/env.py
+++ alembic/env.py
@@ -29,15 +29,17 @@ target_metadata = Base.metadata
 # ... etc.
 
 
-def fix_dialect(s):
+""" def fix_dialect(s):
     if s.startswith("postgres://"):
         s = s.replace("postgres://", "postgresql://")
     s = s.replace("postgresql://", "postgresql+psycopg2://")
     return s
+ """
 
 
 def get_url():
-    return fix_dialect(os.getenv("DATABASE_URL"))
+    # return fix_dialect(os.getenv("DATABASE_URL"))
+    return os.getenv("DATABASE_URL")
 
 
 def run_migrations_offline():
