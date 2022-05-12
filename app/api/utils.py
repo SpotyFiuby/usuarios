8d347e0b47848e60db9e69a83c50b4f07f08ab37
Cynthia Gamarra <cgamarra@fi.uba.ar>
2022-05-12 02:33:46 -0300

    Adding util script for token stuff

---
+++ app/api/utils.py
@@ -0,0 +1,45 @@
+from fastapi import Depends, HTTPException, status, Response
+import os
+from datetime import timedelta
+from jose import JWTError, jwt
+
+from app.schemas.token import TokenPayload
+
+from app.crud.crud_users import users as users_crud
+from app.core.security import ALGORITHM, SECRET_KEY, create_access_token, oauth2_scheme
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+
+def generate_token(db, username, password):
+    user = users_crud.authenticate(db, username, password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email/username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    return create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+
+
+def get_current_user(db, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenPayload(username=username)
+    except JWTError:
+        raise credentials_exception
+
+    user = users_crud.get_by_email(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
