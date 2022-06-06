5701f090026f31b88cfa277aa4376235790a4457
Cynthia Gamarra <cgamarra@fi.uba.ar>
2022-06-06 19:40:52 -0300

    Adding a default for profileImage column

--- app/models/users.py
+++ app/models/users.py
@@ -11,7 +11,10 @@ class Users(Base):
     lastName = Column(String)
     email = Column(String, unique=True, index=True, nullable=False)
     phoneNumber = Column(String, index=True)
-    profileImage = Column(String)
+    profileImage = Column(
+        String,
+        default="https://cdn0.iconfinder.com/data/icons/body-parts-glyph-silhouettes/300/161845119Untitled-3-512.png",
+    )
     isPremium = Column(Boolean, nullable=False, default=False)
     isArtist = Column(Boolean, nullable=False, default=False, server_default='False')
     username = Column(String)
