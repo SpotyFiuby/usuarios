1c6c7727162a6793ccd829ca20d0026926dba29e
luciakasman <lucia.kasman@muttdata.ai>
2022-07-14 20:52:04 -0300

    add codecov settings

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ def tests(session):
     session.install("poetry")
     session.run("poetry", "install", "-E", "testing")
 
-    cmd = ["poetry", "run", "pytest"]
+    cmd = ["poetry", "run", "pytest", "--cov"]
 
     if session.posargs:
         cmd.extend(session.posargs)
